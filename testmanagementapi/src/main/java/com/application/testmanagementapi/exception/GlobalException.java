bfb31c92df51a4b10648658b436830e3bb76684a
ganeshshejwal <ganesh.shejwal@bnt-soft.com>
2024-07-04 12:56:15 +0530

    Update GlobalException.java

--- testmanagementapi/src/main/java/com/application/testmanagementapi/exception/GlobalException.java
+++ testmanagementapi/src/main/java/com/application/testmanagementapi/exception/GlobalException.java
@@ -3,21 +3,22 @@ package com.application.testmanagementapi.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 
 @RestControllerAdvice
 public class GlobalException {
     
-    @org.springframework.web.bind.annotation.ExceptionHandler(QuestionNotFoundException.class)
+    @ExceptionHandler(QuestionNotFoundException.class)
     public ResponseEntity<String> questionNotFoundException(){
         return new ResponseEntity<>("Question is not present in database",HttpStatus.NOT_FOUND);
     }
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(CategoryNotFoundException.class)
+    @ExceptionHandler(CategoryNotFoundException.class)
     public ResponseEntity<String> categoryNotFoundException(CategoryNotFoundException ex){
             return new ResponseEntity<>(ex.getMessage(),HttpStatus.NOT_FOUND);
     }
-
+    @ExceptionHandler(SubCategoryNotFoundException.class)
     public ResponseEntity<String> subCategoryNotFoundException(SubCategoryNotFoundException ex){
         return new ResponseEntity<>(ex.getMessage(),HttpStatus.NOT_FOUND);
     }
