8e46402908de470fa91f3943bed3db9e4b708d14
ganeshshejwal <ganesh.shejwal@bnt-soft.com>
2024-07-04 12:44:04 +0530

    Change Categories to Category

--- testmanagementapi/src/main/java/com/application/testmanagementapi/controller/CategoryController.java
+++ testmanagementapi/src/main/java/com/application/testmanagementapi/controller/CategoryController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/testapp/categories")
+@RequestMapping("/api/testapp/category")
 public class CategoryController {
 
     @Autowired
