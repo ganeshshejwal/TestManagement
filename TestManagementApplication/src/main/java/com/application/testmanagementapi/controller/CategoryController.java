030567c5ad5e8361cd0235c3af36dc7231ec4e21
ganesh.shejwal <ganesh.shejwal@bnt-soft.com>
2024-07-05 21:20:02 +0530

    Change name of project form testmanagementapi to TestManagementApplication

--- TestManagementApplication/src/main/java/com/application/testmanagementapi/controller/CategoryController.java
+++ TestManagementApplication/src/main/java/com/application/testmanagementapi/controller/CategoryController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import com.application.testmanagementapi.exception.CategoryNotFoundException;
 import com.application.testmanagementapi.model.CategoryModel;
 import com.application.testmanagementapi.service.CategoryService;
-
+import org.slf4j.*;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,24 +16,29 @@ import java.util.Optional;
 @RequestMapping("/api/testapp/category")
 public class CategoryController {
 
+    private static final Logger logger = LoggerFactory.getLogger(CategoryController.class);
+
     @Autowired
     private CategoryService categoryService;
     
     @PostMapping
     public ResponseEntity<CategoryModel> createCategory(@RequestBody CategoryModel category) {
         CategoryModel createdCategory = categoryService.saveCategory(category);
+        logger.info("Category Created SucessFully");
         return ResponseEntity.status(HttpStatus.CREATED).body(createdCategory);
     }
 
     @GetMapping
     public ResponseEntity<List<CategoryModel>> getAllCategories() {
         List<CategoryModel> categories = categoryService.getAllCategories();
+        logger.info("Categories Fetched SucessFully");
         return ResponseEntity.status(HttpStatus.OK).body(categories);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Optional<CategoryModel>> getCategoryById(@PathVariable("id") int id) {
         Optional<CategoryModel> category = categoryService.getCategoryById(id);
+        logger.info("Category Fetched SucessFully");
         return ResponseEntity.status(HttpStatus.OK).body(category); 
     }
 
