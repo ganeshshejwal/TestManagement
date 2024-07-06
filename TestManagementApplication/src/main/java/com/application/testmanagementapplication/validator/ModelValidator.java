ff0c6a57caca18bcfa272dfc50fef049ad1cc366
ganesh.shejwal <ganesh.shejwal@bnt-soft.com>
2024-07-07 00:15:16 +0530

    Modify ModelValidator file

--- TestManagementApplication/src/main/java/com/application/testmanagementapplication/validator/ModelValidator.java
+++ TestManagementApplication/src/main/java/com/application/testmanagementapplication/validator/ModelValidator.java
@@ -3,6 +3,7 @@ package com.application.testmanagementapplication.validator;
 import org.springframework.stereotype.Component;
 
 import com.application.testmanagementapplication.model.Category;
+import com.application.testmanagementapplication.model.McqQuestion;
 import com.application.testmanagementapplication.model.SubCategory;
 
 @Component
@@ -25,5 +26,21 @@ public class ModelValidator {
            subCategory.getSubCategoryDescription().isEmpty();
     }
 
+    public boolean isMcqQuestionValid(McqQuestion mcqQuestion) {
+        return mcqQuestion != null &&
+           isNotEmpty(mcqQuestion.getQuestion()) &&
+           isNotEmpty(mcqQuestion.getOptionOne()) &&
+           isNotEmpty(mcqQuestion.getOptionTwo()) &&
+           isNotEmpty(mcqQuestion.getOptionThree()) &&
+           isNotEmpty(mcqQuestion.getOptionFour()) &&
+           isNotEmpty(mcqQuestion.getCorrectOption()) &&
+           isNotEmpty(mcqQuestion.getPositiveMark()) &&
+           isNotEmpty(mcqQuestion.getNegativeMark()) &&
+           mcqQuestion.getSubCategory() != null;
+    }
+
+    private boolean isNotEmpty(String str) {
+        return str != null && !str.isEmpty();
+    }
 
 }
